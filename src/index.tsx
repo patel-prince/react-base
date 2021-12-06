ddb1b2d8bc9b747e619cd8548ce58a264c213fa2
Prince <prpatel0949@gmail.com>
2021-12-06 11:28:27 +0530

    RB-4

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.less'
 import App from './App'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+
+import './assets/less/main.less'
 
 ReactDOM.render(
   <React.StrictMode>
